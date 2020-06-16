86e2d34442d34e581721c1e3c908af1f2e7913f5
Hamish Coleman <hamish@zot.org>
2020-06-16 19:35:05 +0100

    Some keyboard translations can cause a hang - remember to dectrment our stuff pointer

--- vt.c
+++ vt.c
@@ -73,6 +73,7 @@ int kget(int w)
         	for (x = 0; x != stuff_ptr - 1; ++x) {
         		stuff[x] = stuff[x + 1];
         	}
+		stuff_ptr--;
         	stuff[x] = 0;
         	return c;
         }
